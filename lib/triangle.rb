eada9ac6de08b91e91ba17481b5e9b7157e1aaa7
Iryna Stein <isophiestein@gmail.com>
2021-07-18 18:37:08 -0600

    commit tree dd8e491ae9315e380d76a4f8d9bb6d91da1edda2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  # attr_reader :side, :side1, :side2
+
+  def initialize(side, side1, side2)
+    @side = side
+    @side1 = side1
+    @side2 = side2
+  end
+
+  def kind
+    self.validate_triangle
+    if 
+      @side == @side1 && @side1 == @side2
+      :equilateral
+    elsif @side == @side1 || @side == @side2 || @side1 == @side2
+      :isosceles
+    else
+      @side != @side1 && @side1 != @side2
+      :scalene
+    end
+  end
+
+  def greater_than_zero?
+    [@side, @side1, @side2].all? { |side| side > 0 }
+  end
+
+  def valid_triangle?
+    @side + @side1 > @side2 && @side1 + @side2 > @side && @side2 + @side > @side1
+  end
+
+  def validate_triangle
+    if !(greater_than_zero? && valid_triangle?)
+    raise TriangleError 
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
